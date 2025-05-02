84a35e73fbe944d0dcd2a6990224b1b0ef60c73d
Marco Dacio <marco.dacio@icloud.com>
2025-05-03 01:10:35 +0200

    cleaan

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc"; // statt @vitejs/plugin-react
-import path from "path";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import tsconfigPaths from "vite-tsconfig-paths"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  plugins: [
+    react(),
+    tsconfigPaths(), // 👈 holt sich die Pfade automatisch aus tsconfig
+  ],
+})
