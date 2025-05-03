f8377087e08cdd1a01dd2d3d5e61ac0b15824981
Marco Dacio <marco.dacio@icloud.com>
2025-05-03 12:14:07 +0200

    dd

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react-swc"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
+  base: '/tradicoes-harburg-folclore-web/', // 👈 wichtig für GitHub Pages!
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    tsconfigPaths(), // 👈 holt sich die Pfade automatisch aus tsconfig
+    tsconfigPaths(),
   ],
 })
